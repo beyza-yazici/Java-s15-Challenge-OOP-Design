e3a19fc4fdb8c6367e21bc99b39fd1f3331f0996
beyza-yazici <beyzanyazicii@gmail.com>
2025-03-25 22:07:43 +0300

    Models package created. Book, Invoice, Librarian, Library, Person and Reader classes created.

---
+++ src/models/Librarian.java
@@ -0,0 +1,53 @@
+package models;
+
+import java.util.List;
+
+public class Librarian extends Person{
+
+    private String password;
+
+    public Librarian(int id, String name, String email, String password) {
+        super(id, name, email);
+        this.password = password;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public Book searchBook(List<Book> books, String title){
+        for(Book book : books){
+            if(book.getTitle().equalsIgnoreCase(title)){
+                return book;
+            }
+        }
+        return null;
+    }
+
+    public boolean verifyMember(List<Reader> readers, int memberId){
+        for(Reader reader : readers){
+            if(reader.getId() == memberId){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean issueBook(Book book, Reader reader){
+        return reader.borrowBook(book);
+    }
+
+    public double calculateFine(int daysLate){
+        double finePerDay = 1.0;
+        return daysLate * finePerDay;
+    }
+
+    public void createBill(Reader reader, Book book, double amount){
+        System.out.println("Bill created for " + reader.getName() + " for book " + book.getTitle() + ": $" + amount);
+    }
+
+    public void returnBook(Book book, Reader reader) {
+        reader.returnBook(book);
+    }
+
+}
---
+++ src/models/Person.java
@@ -0,0 +1,38 @@
+package models;
+
+public abstract class Person {
+    protected int id;
+    protected String name;
+    protected String email;
+
+    public Person(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String whoYouAre() {
+        return "ID: " + id + ", Name: " + name + ", Email: " + email;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
